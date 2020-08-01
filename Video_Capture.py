93d5340013915d69a9e8a29045d8c81014aa14d2
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-08-01 19:42:36 +0530

    Video Capture
    This code shows you how to recording a video using the webcam and show it.

---
+++ Video_Capture.py
@@ -0,0 +1,24 @@
+import cv2, time
+
+video = cv2.VideoCapture(0)
+
+a=1
+
+while True:
+    a=a+1
+    check, frame = video.read()
+
+    print(check)
+    print(frame)
+
+    cv2.imshow("Capturing", frame)
+
+    key = cv2.waitKey(1)
+    
+    if key == ord('q'):
+        break
+    
+print(a)
+    
+video.release()
+cv2.destroyAllWindows()
